036e1181a8e29a406b6da98a8afe3d0e94b6d063
ПК <kyaacdc2@gmail.com>
2025-12-29 14:26:47 +0000

    Add project files.

---
+++ Hello my world/Hello my world.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string text;
+
+    ifstream input("input.txt");
+    getline(input, text);
+    input.close();
+
+    text.insert(6, "my ");
+
+    ofstream output("output.txt");
+    output << text;
+    output.close();
+
+    cout << "Result: " << text << endl;
+
+    return 0;
+}
