15249ee4888e1755190f6a87b8a39c335de418c8
ПК <kyaacdc2@gmail.com>
2025-12-31 18:29:48 +0000

    ыыы

--- Hello my world/Hello my world.cpp
+++ Hello my world/Hello my world.cpp
@@ -6,16 +6,27 @@ using namespace std;
 
 int main() {
     string text;
+    int n; 
 
     ifstream input("input.txt");
     getline(input, text);
     input.close();
-    size_t firstSpace = text.find(' ');
-    size_t secondSpace = text.find(' ', firstSpace + 1);
 
-    if (secondSpace != string::npos) {
-        text.insert(secondSpace, " beautefful");
+    cout << "Enter word number after which to insert: ";
+    cin >> n;
+
+    size_t pos = 0;
+
+    for (int i = 1; i < n; i++) {
+        pos = text.find(' ', pos);
+        if (pos == string::npos) {
+            cout << "Not enough words in text!" << endl;
+            return 0;
+        }
+        pos++; 
     }
+
+    text.insert(pos - 1, " beautefful");
 
     ofstream output("output.txt");
     output << text;
